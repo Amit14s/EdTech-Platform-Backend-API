a999a27ee11ec7bd2375fafa3195890460ef13cb
amit kumar <amitmouar098@gmail.com>
2026-01-26 15:56:34 +0530

    updated database

--- config/user.db.js
+++ config/user.db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 const MONGOURL=process.env.MONGO_URL;
 const connecttodb=async()=>{
     try{
-        const conn=mongoose.connect(MONGOURL);
+        const conn=mongoose.connect(MONGOURL)||'mongodb+srv://amitmouar098:Amit%40123@m0.zzp5hsu.mongodb.net/?appName=M0';
         if(conn){
             console.log("user database is connected");
         }
